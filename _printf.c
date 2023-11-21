c0c6077a5415376634374e70898e74fac81a0b97
Ferlandoo <contact@ferlando.me>
2023-11-21 02:27:16 -0800

    first code

--- _printf.c
+++ _printf.c
@@ -8,53 +8,53 @@
 
 int _printf(const char *format, ...)
 {
-	va_list list;
-	int i, j, len = 0;
-	int (*f)(va_list);
+    va_list list;
+    int i, j, len = 0;
+    int (*f)(va_list);
 
-	va_start(list, format);
-	i = 0;
-	if (format != NULL)
-	{
-		while (format[i] != '\0')
-		{
-			if (format[i] == '%')
-			{
-				i++;
-				if (format[i] == '\0')
-				{
-					return (-1);
-				}
-				if (format[i] == '%')
-				{
-					putchar('%');
-					len++;
-				}
-				else
-				{
-					f = get_function(format[i]);
-					if (f == NULL)
-					{
-						putchar('%');
-						putchar(format[i]);
-						len += 2;
-					}
-					else
-					{
-						j = f(list);
-						len += j;
-					}
-				}
-			}
-			else
-			{
-				putchar(format[i]);
-				len++;
-			}
-			i++;
-		}
-		va_end(list);
-		return (len);
-	}
-	return (-1);
+    va_start(list, format);
+    i = 0;
+    if (format != NULL)
+    {
+        while (format[i] != '\0')
+        {
+            if (format[i] == '%')
+            {
+                i++;
+                if (format[i] == '\0')
+                {
+                    return (-1);
+                }
+                if (format[i] == '%')
+                {
+                    putchar('%');
+                    len++;
+                }
+                else
+                {
+                    f = get_function(format[i]);
+                    if (f == NULL)
+                    {
+                        putchar('%');
+                        putchar(format[i]);
+                        len += 2;
+                    }
+                    else
+                    {
+                        j = f(list);
+                        len += j;
+                    }
+                }
+            }
+            else
+            {
+                putchar(format[i]);
+                len++;
+            }
+            i++;
+        }
+        va_end(list);
+        return (len);
+    }
+    return (-1);
 }

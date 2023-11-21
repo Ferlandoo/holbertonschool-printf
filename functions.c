03945b8bb41338325b1cb2449691a2d4ae218cdd
blerim <blerimballiu19@gmail.com>
2023-11-21 01:03:05 -0800

    part5

--- functions.c
+++ functions.c
@@ -20,4 +20,60 @@ int printf_char(va_list char_list)
  * @string_list: string to print
  * Return: length of string
  */
+int printf_string(va_list string_list)
+{
+	char *str;
+	int i;
+	int len;
 
+	str = va_arg(string_list, char*);
+	if (str == NULL)
+	{
+	str = "(NULL)";
+	}
+	len = _strlen(str);
+	for (i = 0; i < len; i++)
+	{
+	_putchar(str[i]);
+	}
+	return (len);
+}
+/**
+* _strlen - length of characters on s
+* @s: number of characters
+* Return: length of characters
+*/
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != 0)
+	{
+	i++;
+	}
+	return (i);
+}
+/**
+* get_function - gets the function
+* @c: character to check
+* Return: function
+*/
+int (*get_function(char c))(va_list)
+{
+	int i = 0;
+	print_data prints[] = {
+	{"c", printf_char},
+	{"s", printf_string},
+	{"i", printf_int},
+	{"d", printf_int},
+	{NULL, NULL}
+	};
+	while (prints[i].type)
+	{
+	if (*prints[i].type == c)
+		return (prints[i].print);
+	i++;
+	}
+	return (NULL);
+}
---
+++ main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * struct print_data - structure for printing data types
+ * @type: type of data
+ * @print: function to print data
+ */
+typedef struct print_data
+{
+	char *type;
+	int (*print)(va_list list);
+} print_data;
+int _putchar(char c);
+int _printf(const char *format, ...);
+int printf_char(va_list char_list);
+int printf_string(va_list string_list);
+int _strlen(char *s);
+int (*get_function(char c))(va_list);
+#endif

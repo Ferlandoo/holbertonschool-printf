cc5375109d3dc298a2f39e2280e131738a5b6bda
Ferlandoo <contact@ferlando.me>
2023-11-21 01:31:32 -0800

    fix func

--- functions.c
+++ functions.c
@@ -75,3 +75,59 @@ int (*get_function(char c))(va_list)
 	}
 	return (NULL);
 }
+
+/**
+ * _printf - prints anything
+ * @format: list of argument types passed to the function
+ * Return: number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+    va_list list;
+    int i, j, len = 0;
+    int (*f)(va_list);
+
+    va_start(list, format);
+    i = 0;
+    while (format[i] != 0)
+    {
+            if (format[i] == '%')
+            {
+                    i++;
+                    if (format[i] == '%')
+                    {
+                            _putchar('%');
+                            len++;
+                    }
+                    else if (format[i] != 0)
+                    {
+                            f = get_function(format[i]);
+                            if (f == NULL)
+                            {
+                                    _putchar('%');
+                                    _putchar(format[i]);
+                                    len += 2;
+                            }
+                            else
+                            {
+                                    j = f(list);
+                                    len += j;
+                            }
+                    }
+                    else
+                    {
+                            _putchar(format[i]);
+                            len++;
+                    }
+            }
+            else
+            {
+                    _putchar(format[i]);
+                    len++;
+            }
+            i++;
+    }
+    va_end(list);
+    return (len);
+}
